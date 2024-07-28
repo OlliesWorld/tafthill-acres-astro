f3cfe2c6ba5050fd1f47adde45af43487134f487
Roni <rtlockwoodwork@gmail.com>
2024-07-28 16:20:00 -0600

    add tailwind styles to html

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./public/**/*.html',
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			rotate: {
