e0f82a4eddc8b300c99f8ebb5415a210ee41aa2c
roni <rtlockwoodwork@gmail.com>
2025-04-21 12:39:24 -0600

    fix nav desktop

--- src/pages/api/draft.js
+++ src/pages/api/draft.js
@@ -4,7 +4,7 @@ export async function GET(context) {
   // Parse the "token" cookie
   const url = new URL(context.request.url);
   const token = url.searchParams.get("token");
-  const slug = url.searchParams.get("slug");
+  // const slug = url.searchParams.get("slug");
   const redirectTo = url.searchParams.get("redirectTo") || "/";
 
   if (token !== "MY_SECRET_DRAFT_TOKEN") {
